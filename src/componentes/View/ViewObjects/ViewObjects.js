381ceaf5ceb9d73317d99b5370a1ce1b11a44293
tudodepende <leonasouza.dev@gmail.com>
2021-06-30 18:17:56 -0300

    remocao de navlink desnecessario

--- src/componentes/View/ViewObjects/ViewObjects.js
+++ src/componentes/View/ViewObjects/ViewObjects.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useHistory, NavLink } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { apartamentoModelDetalhes } from "../../../models/Apartamento";
 import { moradorModelDetalhes } from "../../../models/Morador";
 import { veiculoModelDetalhes } from "../../../models/Veiculo";
